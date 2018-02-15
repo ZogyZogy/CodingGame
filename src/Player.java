0314d053db06e364d4c60f86f71e85b630998fbd
zog <zog>
2018-02-15 11:39:26 +0100

    Terminé l'implémentation du A* pour le retour + big refactor du tout

--- src/Player.java
+++ src/Player.java
@@ -5,8 +5,9 @@ import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.IntStream;
 
-public class Player {
+class Player {
 
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
@@ -41,19 +42,24 @@ public class Player {
 		int height;
 		int fuelUsed = 0;
 		int alarmTurns;
-		Position kirkPos;
+		Position kirkPosition;
 		List<String> allSquares = new ArrayList<>();
 		boolean isMapExplored = false; // Phase 1 - Exploration of the map
 		boolean isAlarmTriggered = false; // Phase 2 - Go to Command Room ; Phase 3 - Return to start position 
+		Position commandRoomPosition;
+		PathPosition commandRoomPath;
+		Position startingPosition;
+		PathPosition startingPath;
 
 		public Map(int nbOfColumns, int nbOfRows, int alarmTurns) {
 			this.width = nbOfColumns;
 			this.height = nbOfRows;
 			this.alarmTurns = alarmTurns;
+			IntStream.range(0, nbOfRows).forEach(i -> allSquares.add(""));
 		}
 
 		public void updateKirkPosition(int kirkColum, int kirkRow) {
-			kirkPos = Position.of(kirkColum, kirkRow);
+			kirkPosition = Position.of(kirkColum, kirkRow);
 		}
 
 		public void updateRow(int i, String row) {
@@ -62,16 +68,16 @@ public class Player {
 
 		public String getNextMove() {
 			Position pos = computeNextMove();
-			if (pos.x - kirkPos.x == 1 && pos.y - kirkPos.y == 0) {
+			if (pos.x - kirkPosition.x == 1 && pos.y - kirkPosition.y == 0) {
 				return "RIGHT";
 			}
-			if (pos.x - kirkPos.x == -1 && pos.y - kirkPos.y == 0) {
+			if (pos.x - kirkPosition.x == -1 && pos.y - kirkPosition.y == 0) {
 				return "LEFT";
 			}
-			if (pos.y - kirkPos.y == 1 && pos.x - kirkPos.x == 0) {
+			if (pos.y - kirkPosition.y == 1 && pos.x - kirkPosition.x == 0) {
 				return "UP";
 			}
-			if (pos.y - kirkPos.y == -1 && pos.x - kirkPos.x == 0) {
+			if (pos.y - kirkPosition.y == -1 && pos.x - kirkPosition.x == 0) {
 				return "DOWN";
 			}
 			throw new RuntimeException("Erreur dans la transformation movement en string");
@@ -79,106 +85,182 @@ public class Player {
 
 		public Position computeNextMove() {
 			if (!isMapExplored) {
-				Optional<Position> pos = computeNextExplorationMove();
-				if (pos.isPresent()) {
+				Optional<PathPosition> explorationPath = computeMoveToNearest('?');
+				if (explorationPath.isPresent()) {
+					Optional<Position> pos = explorationPath.get().computeNextMoveInPathFrom(kirkPosition);
 					return pos.get();
 				} else {
 					isMapExplored = true;
 				}
 			}
-			if (!isAlarmTriggered) {
-				Optional<Position> pos = computeGoToCommandRoomMove();
+			if (isMapExplored && !isAlarmTriggered) {
+				if (commandRoomPosition == null && commandRoomPath == null) {
+					computeGoToCommandRoomPath();
+				}
+				Optional<Position> pos = commandRoomPath.computeNextMoveInPathFrom(kirkPosition);
 				if (pos.isPresent()) {
 					return pos.get();
 				} else {
 					isAlarmTriggered = true;
 				}
 			}
-			return computeReturnToStartMove();
+			if (isMapExplored && isAlarmTriggered) {
+				if (startingPosition == null && startingPath == null) {
+					computeReturnToStartPath();
+				}
+				Optional<Position> pos = commandRoomPath.computeNextMoveInPathFrom(kirkPosition);
+				if (pos.isPresent()) {
+					return pos.get();
+				} else {
+					throw new RuntimeException("Start position already reached, shouldn't happen");
+				}
+			}
+			throw new RuntimeException("This shouldn't be reached in method computeNextMove()");
 		}
 
-		private Optional<Position> computeNextExplorationMove() {
-			PriorityQueue<AlgoPosition> open = new PriorityQueue<>();
+		private Optional<PathPosition> computeMoveToNearest(char targetChar) {
+			PriorityQueue<PathPosition> open = new PriorityQueue<>();
 			Set<Position> closed = new HashSet<>();
-			// init
+
+			open.add(new PathPosition(kirkPosition, 0, null));
 			do {
-				AlgoPosition current = open.poll();
-				closed.add(current);
-				if(getCharAtPosition(current) == '?') {
-					return getFirstMoveOfEndPosition(current);
-				} 
-				for (AlgoPosition neighbour : getNeighbours(current)) {
-					char charPos = getCharAtPosition(neighbour);
-					if (charPos != '#' && charPos != 'C' && !closed.contains(neighbour)) {
-						neighbour.cost = getCost(current, neighbour);
-						neighbour.parent = current;
-						open.add(neighbour);
+				PathPosition current = open.poll();
+				closed.add(current.getPosition());
+				if (getCharAtPosition(current.getPosition()) == targetChar) {
+					return Optional.of(current);
+				}
+				for (Position neighbour : current.getPosition().getManhattanNeighbours()) {
+					if (isPositionToInvestigate(neighbour, closed)) {
+						float heuristics = getAntiSymmetricHeuristics(current.getPosition(), neighbour);
+						open.add(new PathPosition(neighbour, heuristics, current));
 					}
 				}
-
 			} while (!open.isEmpty());
-			return null;
+
+			return Optional.empty();
 		}
 
-		private Optional<Position> getFirstMoveOfEndPosition(AlgoPosition current) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		private float getCost(AlgoPosition current, AlgoPosition neighbour) {
-			// TODO : TESTER si la conversion en float marche correctement
-			if (neighbour.x - current.x == -1) {
-				return (float) (current.cost + 1 - 1.0 / width);
-			}
-			if (neighbour.y - current.y == -1) {
-				return (float) (current.cost + 1 - 1.0 / height);
-			}
-			return current.cost + 1;
-		}
-
-		private List<AlgoPosition> getNeighbours(AlgoPosition current) {
-			List<AlgoPosition> list = new ArrayList<>();
-			list.add(new AlgoPosition(current.addCoordinates(1, 0)));
-			list.add(new AlgoPosition(current.addCoordinates(-1, 0)));
-			list.add(new AlgoPosition(current.addCoordinates(0, 1)));
-			list.add(new AlgoPosition(current.addCoordinates(0, -1)));
-			return list;
+		private boolean isPositionToInvestigate(Position pos, Set<Position> closed) {
+			char charPos = getCharAtPosition(pos);
+			return charPos != '#' && charPos != 'C' && !closed.contains(pos);
 		}
 
 		private char getCharAtPosition(Position pos) {
-			return allSquares.get(pos.x).charAt(pos.y);
+			return allSquares.get(pos.y).charAt(pos.x);
 		}
 
-		private Optional<Position> computeGoToCommandRoomMove() {
-			// If already on CommandRoom, return null;
-			return null;
+		private void computeGoToCommandRoomPath() {
+			commandRoomPosition = computeMoveToNearest('C').get().getPosition();
+			commandRoomPath = computePathForEnd(commandRoomPosition);
 		}
 
-		private Position computeReturnToStartMove() {
-			// TODO Auto-generated method stub
-			return null;
+		private void computeReturnToStartPath() {
+			startingPosition = computeMoveToNearest('S').get().getPosition();
+			startingPath = computePathForEnd(commandRoomPosition);
+		}
+
+		private PathPosition computePathForEnd(Position targetPosition) {
+			PriorityQueue<PathPosition> open = new PriorityQueue<>();
+			Set<Position> closed = new HashSet<>();
+
+			open.add(new PathPosition(kirkPosition, 0, null));
+			do {
+				PathPosition current = open.poll();
+				closed.add(current.getPosition());
+				if (targetPosition.equals(current.getPosition())) {
+					return current;
+				}
+				for (Position neighbour : current.getPosition().getManhattanNeighbours()) {
+					if (isPositionToInvestigate(neighbour, closed)) {
+						float heuristics = getHeuristics(neighbour, targetPosition);
+						open.add(new PathPosition(neighbour, heuristics, current));
+					}
+				}
+			} while (!open.isEmpty());
+
+			throw new RuntimeException("Target position " + targetPosition + " not found !");
+		}
+
+		private float getAntiSymmetricHeuristics(Position current, Position next) {
+			// TODO : TESTER si la conversion en float marche correctement
+			if (next.x - current.x < 0) {
+				return (float) (-1.0 / width);
+			}
+			if (next.y - current.y < 0) {
+				return (float) (-1.0 / height);
+			}
+			return 0;
+		}
+
+		private float getHeuristics(Position neighbour, Position targetPosition) {
+			int distanceX = Math.abs(targetPosition.x - neighbour.x);
+			int distanceY = Math.abs(targetPosition.y - neighbour.y);
+			float antiSymmetry = getAntiSymmetricHeuristics(neighbour, targetPosition);
+			return antiSymmetry + distanceX + distanceY;
 		}
 
 	}
 
-	public static class AlgoPosition extends Position implements Comparable<AlgoPosition> {
+	public static class PathPosition implements Comparable<PathPosition> {
 
-		private AlgoPosition parent;
-		private float cost;
+		private final PathPosition parent;
+		private final Position position;
+		private final float total; // equals to f in A*
+		private final int cost; // equals to g in A*
 
-		public AlgoPosition(Position position) {
-			super(position);
+		public PathPosition(Position position, float heuristics, PathPosition parent) {
+			this.position = position;
+			this.parent = parent;
+			if (parent != null) {
+				this.cost = parent.cost + 1;
+				this.total = this.cost + heuristics;
+			} else {
+				this.cost = 0;
+				this.total = 0;
+			}
 		}
 
-		public AlgoPosition(Position position, float cost, AlgoPosition parent) {
-			super(position);
-			this.cost = cost;
-			this.parent = parent;
+		public Optional<Position> computeNextMoveInPathFrom(Position currentPosition) {
+			System.err.println("Current Pos : " + currentPosition);
+			System.err.println("Target Pos : " + this);
+			if (currentPosition.equals(this.position)) {
+				return Optional.empty();
+			}
+			PathPosition tempPathPosition = this;
+			while (!currentPosition.equals(tempPathPosition.parent.getPosition())) {
+				tempPathPosition = this.parent;
+				System.err.println("Previous Pos : " + currentPosition);
+			}
+
+			return Optional.of(tempPathPosition.getPosition());
+		}
+
+		public Position getPosition() {
+			return position;
 		}
 
 		@Override
-		public int compareTo(AlgoPosition other) {
-			return Float.compare(this.cost, other.cost);
+		public String toString() {
+			return position.toString() + " ; cost : " + cost;
+		}
+
+		@Override
+		public int compareTo(PathPosition other) {
+			return Float.compare(this.total, other.total);
+		}
+
+		@Override
+		public int hashCode() {
+			return (int) (this.cost * 17 + 73);
+		}
+
+		@Override
+		public boolean equals(Object object) {
+			if (object == null) {
+				return false;
+			}
+			PathPosition pos = (PathPosition) object;
+			return pos.cost == this.cost;
 		}
 
 	}
@@ -216,6 +298,15 @@ public class Player {
 
 		public Position addCoordinates(int x, int y) {
 			return new Position(this.x + x, this.y + y);
+		}
+
+		public List<Position> getManhattanNeighbours() {
+			List<Position> list = new ArrayList<>();
+			list.add(this.addCoordinates(1, 0));
+			list.add(this.addCoordinates(-1, 0));
+			list.add(this.addCoordinates(0, 1));
+			list.add(this.addCoordinates(0, -1));
+			return list;
 		}
 
 		@Override
